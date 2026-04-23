90e4e5235cff430314a8bf8781eb6832852dd779
ladybeverly <bbyrnea@gmail.com>
2026-04-23 19:34:50 -0400

    average step count calculation

---
+++ Step Tracker/Extensions/Date+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  Date+Ext.swift
+//  Step Tracker
+//
+//  Created by Beverly Byrne on 4/23/26.
+//
+
+import Foundation
+
+extension Date {
+    var weekdayInt: Int {
+        Calendar.current.component(.weekday, from: self)
+    }
+}
