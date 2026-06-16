f833deb4bcd179c483247766077610142187c2fd
ladybeverly <bbyrnea@gmail.com>
2026-06-16 12:57:15 -0400

    project reorganization

--- Step Tracker/Charts/Chart Utilities/ChartHelper.swift
+++ Step Tracker/Charts/Chart Utilities/ChartHelper.swift
@@ -1,15 +1,24 @@
 //
-//  ChartMath.swift
+//  ChartHelper.swift
 //  Step Tracker
 //
-//  Created by Beverly Byrne on 4/23/26.
+//  Created by Beverly Byrne on 6/10/26.
 //
 
 import Foundation
 import Algorithms
 
-struct ChartMath {
+struct ChartHelper {
+    static func convert(data: [HealthMetric]) -> [DateValueChartData] {
+        data.map { .init(date: $0.date, value: $0.value)}
+    }
     
+    static func parseSelectedData(from data: [DateValueChartData], in selectedDate: Date?) -> DateValueChartData? {
+        guard let selectedDate else { return nil }
+        return data.first {
+            Calendar.current.isDate(selectedDate, inSameDayAs: $0.date)
+        }
+    }
     static func averageWeekdayCount(for metric: [HealthMetric]) -> [DateValueChartData] {
         let sortedByWeekday = metric.sorted(using: KeyPathComparator(\.date.weekdayInt))
         let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt == $1.date.weekdayInt }
